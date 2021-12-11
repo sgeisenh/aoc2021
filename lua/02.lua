dbfeccb6f1e8ecb3f060a86c6312f9cdff84374e
Samuel Eisenhandler <sgeisenhandler@gmail.com>
2021-12-11 13:23:08 -0500

    Formatting

--- lua/02.lua
+++ lua/02.lua
@@ -3,41 +3,41 @@ require 'util'
 lines = split(readall('../input/02.txt'), '\n')
 
 function partOne(directions)
-  local hor, depth
-  hor = 0
-  depth = 0
-  for _, line in ipairs(lines) do
-    splits = split(line, ' ')
-    local dist = tonumber(splits[2])
-    if splits[1] == 'forward' then
-      hor = hor + dist
-    elseif splits[1] == 'down' then
-      depth = depth + dist
-    elseif splits[1] == 'up' then
-      depth = depth - dist
+    local hor, depth
+    hor = 0
+    depth = 0
+    for _, line in ipairs(lines) do
+        splits = split(line, ' ')
+        local dist = tonumber(splits[2])
+        if splits[1] == 'forward' then
+            hor = hor + dist
+        elseif splits[1] == 'down' then
+            depth = depth + dist
+        elseif splits[1] == 'up' then
+            depth = depth - dist
+        end
     end
-  end
-  return hor * depth
+    return hor * depth
 end
 
 function partTwo(directions)
-  local hor, depth, aim
-  hor = 0
-  depth = 0
-  aim = 0
-  for _, line in ipairs(lines) do
-    splits = split(line, ' ')
-    local dist = tonumber(splits[2])
-    if splits[1] == 'forward' then
-      hor = hor + dist
-      depth = depth + aim * dist
-    elseif splits[1] == 'down' then
-      aim = aim + dist
-    elseif splits[1] == 'up' then
-      aim = aim - dist
+    local hor, depth, aim
+    hor = 0
+    depth = 0
+    aim = 0
+    for _, line in ipairs(lines) do
+        splits = split(line, ' ')
+        local dist = tonumber(splits[2])
+        if splits[1] == 'forward' then
+            hor = hor + dist
+            depth = depth + aim * dist
+        elseif splits[1] == 'down' then
+            aim = aim + dist
+        elseif splits[1] == 'up' then
+            aim = aim - dist
+        end
     end
-  end
-  return hor * depth
+    return hor * depth
 end
 
 print('Part one:', partOne(lines))
