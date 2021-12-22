8a59e8ac3400b9b6eebc8888baddf447881b83a2
Samuel Eisenhandler <sgeisenhandler@gmail.com>
2021-12-22 02:21:33 -0500

    Swap to quicker subcuboid implementation for 22

--- python/22.py
+++ python/22.py
@@ -1,5 +1,4 @@
 import fileinput
-import copy
 
 lines = [line.strip() for line in fileinput.input()]
 
@@ -14,55 +13,50 @@ for line in lines:
   z1, z2 = z
   steps.append((ison, x1, x2, y1, y2, z1, z2))
 
-cubes = set()
-for step in steps:
-  ison, x1, x2, y1, y2, z1, z2 = step
-  if x1 > 50 or x2 < -50 or y1 > 50 or y2 < -50 or z1 > 50 or z2 < -50:
-    continue
-  for x in range(x1, x2 + 1):
-    for y in range(y1, y2 + 1):
-      for z in range(z1, z2 + 1):
-        if ison:
-          cubes.add((x, y, z))
-        elif (x, y, z) in cubes:
-          cubes.remove((x, y, z))
+def doIntersect(c1, c2):
+  x11, x12, y11, y12, z11, z12 = c1
+  x21, x22, y21, y22, z21, z22 = c2
+  return not (x11 > x22 or x21 > x12 or y11 > y22 or y21 > y12 or z11 > z22 or z21 > z12)
 
-print('Part one:', len([(x, y, z) for (x, y, z) in cubes if -50 <= x <= 50 and -50 <= y <= 50 and -50 <= z <= 50]))
-
-def intersection(c1, c2):
+def remove(c1, c2):
   x11, x12, y11, y12, z11, z12 = c1
   x21, x22, y21, y22, z21, z22 = c2
   x31, x32 = max(x11, x21), min(x12, x22)
   y31, y32 = max(y11, y21), min(y12, y22)
   z31, z32 = max(z11, z21), min(z12, z22)
-  return x31, x32, y31, y32, z31, z32
-
+  result = [(x11, x12, y11, y31 - 1, z11, z12), # bottom
+            (x11, x12, y32 + 1, y12, z11, z12), # top
+            (x11, x31 - 1, y31, y32, z11, z12), # left
+            (x32 + 1, x12, y31, y32, z11, z12), # right
+            (x31, x32, y31, y32, z11, z31 - 1), # front
+            (x31, x32, y31, y32, z32 + 1, z12)] # back
+  result = [(x1, x2, y1, y2, z1, z2) for (x1, x2, y1, y2, z1, z2) in result if x1 <= x2 and y1 <= y2 and z1 <= z2]
+  return result
+  
 def cuboidVolume(c):
   x1, x2, y1, y2, z1, z2 = c
   return (x2 - x1 + 1) * (y2 - y1 + 1) * (z2 - z1 + 1)
 
-cuboids = {}
-for step in steps:
-  ison, x1, x2, y1, y2, z1, z2 = step
-  newc = x1, x2, y1, y2, z1, z2
-  new_cuboids = copy.deepcopy(cuboids)
-  for cbd, count in cuboids.items():
-    ix1, ix2, iy1, iy2, iz1, iz2 = intersection(newc, cbd)
-    if ix1 <= ix2 and iy1 <= iy2 and iz1 <= iz2:
-      icbd = ix1, ix2, iy1, iy2, iz1, iz2
-      if icbd not in new_cuboids:
-        new_cuboids[icbd] = -count
+def stepVolume(steps):
+  cuboids = []
+  for step in steps:
+    ison, x1, x2, y1, y2, z1, z2 = step
+    newc = x1, x2, y1, y2, z1, z2
+    newcuboids = []
+    for cuboid in cuboids:
+      if doIntersect(cuboid, newc):
+        for subc in remove(cuboid, newc):
+          newcuboids.append(subc)
       else:
-        new_cuboids[icbd] -= count
-  if ison:
-    if newc not in new_cuboids:
-      new_cuboids[newc] = 1
-    else:
-      new_cuboids[newc] += 1
-  cuboids = new_cuboids
+        newcuboids.append(cuboid)
+    if ison:
+      newcuboids.append(newc)
+    cuboids = newcuboids
 
-totalVol = 0
-for cuboid, count in cuboids.items():
-  totalVol += count * cuboidVolume(cuboid)
+  totalVol = 0
+  for cuboid in cuboids:
+    totalVol += cuboidVolume(cuboid)
+  return totalVol
 
-print('Part two:', totalVol)
+print('Part one:', stepVolume([(ison, x1, x2, y1, y2, z1, z2) for (ison, x1, x2, y1, y2, z1, z2) in steps if not(x1 > 50 or x2 < -50 or y1 > 50 or y2 < -50 or z1 > 50 or z2 < -50)]))
+print('Part two:', stepVolume(steps))
